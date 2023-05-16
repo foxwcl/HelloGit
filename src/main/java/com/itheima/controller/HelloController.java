998c2678f36b58460c69b01780e39376cff657aa
itcast <hello@itcast.cn>
2023-05-16 20:32:42 +0800

    毛局

--- src/main/java/com/itheima/controller/HelloController.java
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -1,4 +1,7 @@
 package com.itheima.controller;
 
 public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("毛局");
+    }
 }
--- src/main/java/com/itheima/controller/Test1Controller.java
+++ src/main/java/com/itheima/controller/Test1Controller.java
@@ -5,5 +5,6 @@ public class Test1Controller {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好吗？");
+
     }
 }
