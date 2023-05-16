d8ce8ccae728542347bf4013319d0623bf0f5066
itcast <hello@itcast.cn>
2023-05-16 19:56:13 +0800

    添加你好吗

--- src/main/java/com/itheima/controller/Test1Controller.java
+++ src/main/java/com/itheima/controller/Test1Controller.java
@@ -4,5 +4,6 @@ package com.itheima.controller;
 public class Test1Controller {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好吗？");
     }
 }
